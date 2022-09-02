18362a4bf9f6ae295bd76322777488532aa59d6a
wandy <wandywahidin53@gmail.com>
2022-09-02 21:40:35 +0700

    fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='text-center border-b-2 border-gray-400 p-3'>
         <h1 className='text-2xl font-bold'>Front End Developer Test</h1>
-        <h4 className='text-xl font-semibold'>PT. Lewancon Internasional</h4>
+        <h4 className='text-xl font-semibold'>PT. Lawencon Internasional</h4>
     </div>
   )
 }
